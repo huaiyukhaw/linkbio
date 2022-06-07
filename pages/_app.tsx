8441ea7300e7bf1058172eb239a47486406246cc
Huaiyu Khaw <huaiyukhaw@gmail.com>
2022-06-07 19:03:06 +0800

    add safe padding and margin

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { Flowbite } from 'flowbite-react'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Flowbite>
-      <div className="bg-white dark:bg-gray-900 dark:text-white h-screen w-screen p-safe">
+      <div className="bg-white dark:bg-gray-900 dark:text-white h-screen w-screen">
         <Component {...pageProps} />
       </div>
     </Flowbite>
